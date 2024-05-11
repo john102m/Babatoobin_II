a70374e651b30ba9b4478a62e8e8b30534b94846
john mckinney <30315589+john102m@users.noreply.github.com>
2024-05-11 16:18:19 +0100

    add a footer with a map and contact section

---
+++ wwwroot/js/mapicon.js
@@ -0,0 +1,25 @@
+﻿async function myMap() {
+
+    var myCenter = new google.maps.LatLng(55.82965333259990, -4.516478061676025);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = { center: myCenter, zoom: 12 };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker(
+        {
+            position: myCenter,
+            icon: "img/favicon.png",
+            title: "Babatoobins, busy bee",
+        });
+
+    marker.setMap(map);
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById('infobox-content').innerHTML,
+        ariaLabel: "Babatoobins",
+    });
+
+    marker.addListener("click", () => {
+        infoWindow.open({ anchor: marker, map, });
+
+    });
+}
